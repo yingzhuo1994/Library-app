841340a07fe870356c581efccb7332d42b9d8b7a
yingzhuo <yingzhuo1994@gmail.com>
2023-11-30 01:23:52 -0600

    add authentication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,23 +5,50 @@ import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { HomePage } from "./layouts/HomePage/HomePage";
 import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import { BookCheckoutPage } from "./layouts/BookCheckoutPage/BookCheckoutPage";
+import { oktaConfig } from "./lib/oktaConfig";
+import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
+import { Security, LoginCallback, SecureRoute } from "@okta/okta-react";
+import LoginWidget from './Auth/LoginWidget';
+
+const oktaAuth = new OktaAuth(oktaConfig);
 
 export const App = () => {
+  const customAuthHandler = () => {
+    navigate("/login");
+  };
+
+  const navigate = useNavigate();
+
+  const restoreOriginalUri = async (_oktaAuth: any, originalUri: any) => {
+    navigate(toRelativeUrl(originalUri || "/", window.location.origin));
+  };
+
   return (
     <div className="d-flex flex-column min-vh-100">
-      <Navbar />
-      <div className="flex-grow-1">
-        <Routes>
-          {/* <Route path="/" element={<HomePage />} /> */}
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/search" element={<SearchBooksPage />} />
-          <Route path="/checkout/:bookId" element={<BookCheckoutPage />} />
-        </Routes>
-      </div>
-      <Footer />
+      <Security
+        oktaAuth={oktaAuth}
+        restoreOriginalUri={restoreOriginalUri}
+        onAuthRequired={customAuthHandler}
+      >
+        <Navbar />
+        <div className="flex-grow-1">
+          <Routes>
+            {/* <Route path="/" element={<HomePage />} /> */}
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/search" element={<SearchBooksPage />} />
+            <Route path="/checkout/:bookId" element={<BookCheckoutPage />} />
+            <Route
+              path="/login"
+              element={<LoginWidget config={oktaConfig} />}
+            />
+            <Route path="/login/callback" element={<LoginCallback />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Security>
     </div>
   );
 };
