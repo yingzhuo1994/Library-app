b64bfc50074c67a86005f0571f739db10bfd9703
yingzhuo <yingzhuo1994@gmail.com>
2023-12-02 15:43:46 -0600

    Spring Boot Reviews

--- backend/spring-boot-library/src/main/java/com/yingzhuo/springbootlibrary/dao/ReviewRepository.java
+++ backend/spring-boot-library/src/main/java/com/yingzhuo/springbootlibrary/dao/ReviewRepository.java
@@ -14,7 +14,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     Page<Review> findByBookId(@RequestParam("book_id") Long bookId,
                               Pageable pageable);
 
-//    Review findByUserEmailAndBookId(String userEmail, Long bookId);
+    Review findByUserEmailAndBookId(String userEmail, Long bookId);
 
 //    @Modifying
 //    @Query("delete from Review where book_id in :book_id")
